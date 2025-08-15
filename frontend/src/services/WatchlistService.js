ebb5e2478eb31b624165dcd14079a406c3452bbc
Hemanth <hemanthhra12@gmail.com>
2025-08-15 14:53:36 -0300

    localhost to server fix

--- frontend/src/services/WatchlistService.js
+++ frontend/src/services/WatchlistService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const WL = "http://localhost:8080/api/watchlist";
+const WL = "http://3.146.37.153:8080/api/watchlist";
 
 export const getMyWatchlist    = () => axios.get(WL);
 export const addToWatchlist    = (id) => axios.post(`${WL}/${id}`);
